82ec76e6c62ae029fb0cca59c3ec048731a819d5
flak92 <73192248+flak92@users.noreply.github.com>
2023-05-10 16:22:04 +0100

    sekcja 2, odcinek 6 dodanie auth0 jako loginu

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>
-    <h1>Home</h1>
-
-
-
-  </div>;
+  return (
+    <div>
+      <h1>Home PAGE !</h1>
+      <div>
+        <Link href="/api/auth/login">Login</Link>
+      </div>
+    </div>
+  );
 }

1f4d76dc6ad1ef3d46985ae8ea3daa3ff8e26c99
flak92 <73192248+flak92@users.noreply.github.com>
2023-05-11 14:20:41 +0100

    3: 13. Add Logo Component & tailwinded ggle Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
 import "../styles/globals.css";
-import { UserProvider, userProvider } from "@auth0/nextjs-auth0/client";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import {DM_Sans, DM_Serif_Display} from '@next/font/google';
+
+
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: "--font-dm-sans"
+});
+
+const dmSerifDisplay = DM_Serif_Display({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: "--font-dm-serif"
+});
 
 function MyApp({ Component, pageProps }) {
 
@@ -8,7 +22,9 @@ const getLayout = Component.getLayout || ((page) => page);
 
   return (
     <UserProvider>
+      <main className={`${dmSans.variable} ${dmSerifDisplay.variable}`}>
       {getLayout(<Component {...pageProps} />, pageProps)}
+      </main>
     </UserProvider>
   );
 }

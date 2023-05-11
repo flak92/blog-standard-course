e1beb4bfda324805597c9bc6a505ba9261b0d02e
flak92 <73192248+flak92@users.noreply.github.com>
2023-05-11 11:20:29 +0100

    3: 10. = Stylowanie Layoutu

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,7 +1,20 @@
 export const AppLayout = ({ children }) => {
-    return (
-        <div>
-            this is the app layout<div>{children}</div>
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+      <div className="flex flex-col text-white overflow-hidden">
+    
+    <div className="bg-slate-800">
+      <div>logo</div>
+      <div>create post button</div>
+      <div>tokens</div>
+      </div>
+
+      <div className="flex-1 overflow-auto bg-gradient-to-b from-orange-500 to-cyan-300">list of posts</div>
+      <div className="bg-cyan-800">User Info & Logout button</div>
+
+
+      </div>
+      <div className="">{children}</div>
+    </div>
+  );
 };

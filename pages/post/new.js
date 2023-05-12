5f192890909341c53302fc2871e442dc71cefe8b
flak92 <73192248+flak92@users.noreply.github.com>
2023-05-12 09:28:52 +0100

    4:15. Add chatGPT-API, Test Serv & client Terminal

--- pages/post/new.js
+++ pages/post/new.js
@@ -3,9 +3,20 @@ import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost(props) {
   console.log("NEW Post PROPS:", props);
+
+  const handleClick = async () => {
+    const response = await fetch('/api/generatePost', { method: 'POST', });
+    const json = await response.json();
+    console.log("Rezultat:", json);
+  };
+
   return (
     <div>
       <h1>TO JEST NOWY POST</h1>
+
+      <button className="btn" onClick={handleClick}>
+        Generate{" "}
+      </button>
     </div>
   );
 }

c5a171cc57ce3a5bbd64321501eeeb92ab373630
flak92 <73192248+flak92@users.noreply.github.com>
2023-05-12 17:12:30 +0100

    4:18. Chng t GPTturbo & devide APIanswer in parts

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -6,15 +6,11 @@ export default async function handler(req, res) {
   });
   const openai = new OpenAIApi(config);
 
-
-
-  const {topic, keywords} = req.body;
+  const { topic, keywords } = req.body;
   //const topic = "Fullstack-JS-Developer most used functions";
   //const keywords = "change from js to ts, best js stacks, next vs express";
 
-
-
-
+  /*
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     temperature: 0,
@@ -29,9 +25,101 @@ export default async function handler(req, res) {
         "metadescription": post description here
     }`,
   });
-  
-  console.log("response:", response);
+  */
 
-  res.status(200).json({ post: JSON.parse(response.data.choices[0]?.text.split("\n").join("")),
+  const postContentResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a Senior Fullstack JS Developer and you teach only best practices on your blog",
+      },
+      {
+        role: "user",
+        content: `generate a JS teaching best practice SEO friendly post about ${topic},
+      that targets the following comma-separated keywords: ${keywords}. 
+      Content should be formatted in SEO-friendly HTML,
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`,
+      },
+    ],
+  });
+
+  const postContent = postContentResponse.data.choices[0]?.message?.content || "";
+
+  const titleResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a Senior Fullstack JS Developer and you teach only best practices on your blog",
+      },
+      {
+        role: "user",
+        content: `generate a JS teaching best practice SEO friendly post about ${topic},
+      that targets the following comma-separated keywords: ${keywords}. 
+      Content should be formatted in SEO-friendly HTML,
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`,
+      }, {
+        role: "assistant",
+        content: postContent
+      }, {
+        role: "user",
+        content: "Generate appropriate title tag text for the above post"
+      }
+    ],
+  });
+
+  const metaDescriptionResponse = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a Senior Fullstack JS Developer and you teach only best practices on your blog",
+      },
+      {
+        role: "user",
+        content: `generate a JS teaching best practice SEO friendly post about ${topic},
+      that targets the following comma-separated keywords: ${keywords}. 
+      Content should be formatted in SEO-friendly HTML,
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, li, ol, ul, i.`,
+      }, {
+        role: "assistant",
+        content: postContent
+      }, {
+        role: "user",
+        content: "Generate SEO-friendly meta description content for the above post"
+      }
+    ],
+  });
+
+  const title = titleResponse.data.choices[0]?.message?.content || "";
+  const metaDescription = metaDescriptionResponse.data.choices[0]?.message?.content || "";
+
+  console.log("Tu masz Post Content:", postContent);
+  console.log("Tu masz Title:", title);
+  console.log("Tu masz Metadata:", metaDescription);
+
+  /*
+  res
+    .status(200)
+    .json({
+      post: JSON.parse(response.data.choices[0]?.text.split("\n").join("")),
+    });
+    */
+
+res.status(200).json({
+  post: {
+    postContent,
+    title,
+    metaDescription,
+  },
 });
-}
+
+
+};

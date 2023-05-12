828302848401c5561281986f59d8c5225b354439
flak92 <73192248+flak92@users.noreply.github.com>
2023-05-12 19:01:06 +0100

    4:19,20.  Connect MongoDb, auth User & add Tokens

--- pages/token-topup.js
+++ pages/token-topup.js
@@ -1,24 +1,29 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { AppLayout } from '../components/AppLayout';
-
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../components/AppLayout";
 
 export default function TokenTopup() {
-    return <div>
-  
-  
-      <h1>token-topup page</h1>
-  
-  
-    </div>;
-  }
-
-  TokenTopup.getLayout = function getLayout(page, pageProps) {
-    return <AppLayout {...pageProps}>{page}</AppLayout>;
+  const handleClick = async () => {
+    await fetch(`/api/addTokens`, {
+      method: "POST",
+    });
   };
 
-  export const getServerSideProps = withPageAuthRequired (() => {
-    return {
-      props: {
-      },
-    };
-  });
+  return (
+    <div>
+      <h1>token-topup page</h1>
+      <button className="btn" onClick={handleClick}>
+        Add Tokens
+      </button>
+    </div>
+  );
+}
+
+TokenTopup.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
